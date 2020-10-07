1e112810a6a896e554a5fc4683f79a144de9ed7b
Valery Gusev <guza971@gmail.com>
2020-10-07 22:14:54 +0300

    Create main logic

--- ТТТ/Form1.Designer.cs
+++ ТТТ/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -38,20 +37,11 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.MediumAquamarine;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSeaGreen;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(120, 120);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -59,7 +49,7 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSeaGreen;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(138, 12);
+            this.button2.Location = new System.Drawing.Point(137, 61);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(120, 120);
             this.button2.TabIndex = 1;
@@ -72,7 +62,7 @@
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSeaGreen;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(264, 13);
+            this.button3.Location = new System.Drawing.Point(263, 62);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(120, 120);
             this.button3.TabIndex = 2;
@@ -85,7 +75,7 @@
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSeaGreen;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(12, 138);
+            this.button4.Location = new System.Drawing.Point(11, 187);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(120, 120);
             this.button4.TabIndex = 3;
@@ -98,7 +88,7 @@
             this.button5.FlatAppearance.BorderSize = 0;
             this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSeaGreen;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Location = new System.Drawing.Point(139, 139);
+            this.button5.Location = new System.Drawing.Point(138, 188);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(120, 120);
             this.button5.TabIndex = 4;
@@ -111,7 +101,7 @@
             this.button6.FlatAppearance.BorderSize = 0;
             this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSeaGreen;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Location = new System.Drawing.Point(264, 139);
+            this.button6.Location = new System.Drawing.Point(263, 188);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(120, 120);
             this.button6.TabIndex = 5;
@@ -124,7 +114,7 @@
             this.button7.FlatAppearance.BorderSize = 0;
             this.button7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSeaGreen;
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button7.Location = new System.Drawing.Point(13, 264);
+            this.button7.Location = new System.Drawing.Point(12, 313);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(120, 120);
             this.button7.TabIndex = 6;
@@ -137,7 +127,7 @@
             this.button8.FlatAppearance.BorderSize = 0;
             this.button8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSeaGreen;
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button8.Location = new System.Drawing.Point(139, 265);
+            this.button8.Location = new System.Drawing.Point(138, 314);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(120, 120);
             this.button8.TabIndex = 7;
@@ -150,19 +140,55 @@
             this.button9.FlatAppearance.BorderSize = 0;
             this.button9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSeaGreen;
             this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button9.Location = new System.Drawing.Point(264, 264);
+            this.button9.Location = new System.Drawing.Point(263, 313);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(120, 120);
             this.button9.TabIndex = 8;
             this.button9.UseVisualStyleBackColor = false;
             this.button9.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(11, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 120);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(83, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 32);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ходит игрок 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(11, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(372, 51);
+            this.panel1.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.ClientSize = new System.Drawing.Size(398, 399);
+            this.ClientSize = new System.Drawing.Size(394, 446);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -175,7 +201,9 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "      ";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -191,6 +219,8 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- ТТТ/Form1.cs
+++ ТТТ/Form1.cs
@@ -11,17 +11,87 @@ using System.Windows.Forms;
 namespace ТТТ
 {
     public partial class Form1 : Form
-    {
+    { 
+
+        private int player;
+    
         public Form1()
         {
             InitializeComponent();
+            player = 1;
+            label1.Text = "Ходит игрок 1";
+
+
+        
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //MessageBox.Show(sender.GetType().GetProperty("Name").GetValue(sender).ToString());
-            //sender.GetType().GetProperty("text").SetValue(sender, "x");
-            
+            switch (player)
+            {
+                case 1:
+                    sender.GetType().GetProperty("Image").SetValue(sender, Image.FromFile(@"C:\Repositories\ТТТ\ТТТ\icon\cross1.png"));
+                    player = 0;
+                    label1.Text = "Ходит игрок 2";
+
+                    break;
+                case 0:
+                    sender.GetType().GetProperty("Image").SetValue(sender, Image.FromFile(@"C:\Repositories\ТТТ\ТТТ\icon\circle1.png"));
+                    player = 1;
+                    label1.Text = "Ходит игрок 1";
+
+                    break;
+            }
+            //sender.GetType().GetProperty("Enabled").SetValue(sender, false);
+            Button btn = (Button)sender;
+            btn.Click -= new System.EventHandler(this.button1_Click);
+            //checkWin();
         }
+
+        //private void checkWin()
+        //{
+        //    string msg = "Вы победили!";
+
+        //    //Проверка строк
+        //    if ((button1.Image == button2.Image && button2.Image == button3.Image) && (button2.Image != null))
+        //    {
+        //            MessageBox.Show(msg);
+        //    }
+        //    if ((button4.Image == button5.Image && button5.Image == button6.Image) && (button5.Image != null))
+        //    {
+        //            MessageBox.Show(msg);
+        //    }
+        //    if ((button7.Image == button8.Image && button8.Image == button9.Image) && (button8.Image != null))
+        //    {
+        //            MessageBox.Show(msg);
+        //    }
+
+
+        //    //Проверка столбцов
+        //    if ((button1.Image == button4.Image && button4.Image == button7.Image) && (button4.Image != null))
+        //    {
+        //            MessageBox.Show(msg);
+        //    }
+        //    if ((button2.Image == button5.Image && button5.Image == button8.Image) && (button5.Image != null))
+        //    {
+        //            MessageBox.Show(msg);
+        //    }
+        //    if ((button3.Image == button6.Image && button6.Image == button9.Image) && (button6.Image != null))
+        //    {
+        //            MessageBox.Show(msg);
+        //    }
+
+
+        //    //Проверка диагоналей
+        //    if ((button1.Image == button5.Image && button5.Image == button9.Image) && (button5.Image != null))
+        //    {
+        //            MessageBox.Show(msg);
+        //    }
+        //    if ((button3.Image == button5.Image && button5.Image == button7.Image) && (button5.Image != null))
+        //    {               
+        //            MessageBox.Show(msg);
+        //    }      
+        //}
     }
 }
